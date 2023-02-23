c725593710c54774d97c16661bde771ee513596e
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-02-23 14:08:02 -0500

    property of u.s. government

---
+++ lua/weapons/tacint_extras_m1911.lua
@@ -0,0 +1,193 @@
+SWEP.Base = "tacint_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "Colt .45"
+SWEP.Category = "Tactical Intervention Extras (VA1)"
+
+SWEP.Description = "Classic military handgun. Cannot accept tactical attachments but still packs a wallop."
+SWEP.ViewModel = "models/weapons/tacint_extras/v_m1911.mdl"
+SWEP.WorldModel = "models/weapons/tacint_extras/w_m1911.mdl"
+
+SWEP.Slot = 1
+
+// "ballistics"
+
+SWEP.Damage_Max = 33 -- MFW .45 STOPPIN POWAH
+SWEP.Damage_Min = 12
+SWEP.Range_Min = 900
+SWEP.Range_Max = 3000
+SWEP.Penetration = 3 // units of metal this weapon can penetrate
+
+SWEP.MuzzleVelocity = 8000
+
+SWEP.BodyDamageMultipliers = {
+    [HITGROUP_HEAD] = 2,
+    [HITGROUP_CHEST] = 1.25,
+    [HITGROUP_STOMACH] = 1,
+    [HITGROUP_LEFTARM] = 1,
+    [HITGROUP_RIGHTARM] = 1,
+    [HITGROUP_LEFTLEG] = 0.75,
+    [HITGROUP_RIGHTLEG] = 0.75,
+    [HITGROUP_GEAR] = 0.9
+}
+
+// misc. shooting
+
+SWEP.Firemode = 1
+
+SWEP.RPM = 450
+
+SWEP.Spread = 0.0055
+
+SWEP.ShootTimeMult = 0.5
+
+
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 5
+SWEP.RecoilResetTime = 0.3
+SWEP.RecoilDissipationRate = 80
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 0.07
+
+SWEP.RecoilKick = 0.25
+
+SWEP.RecoilSpreadPenalty = 0.005
+
+SWEP.CanBlindFire = true
+
+// handling
+
+SWEP.MoveSpeedMult = 0.95
+SWEP.ShootingSpeedMult = 1
+SWEP.SightedSpeedMult = 0.5
+SWEP.MeleeSpeedMult = 0.5
+SWEP.MeleeSpeedMultTime = 2
+
+SWEP.SprintToFireTime = 0.2
+
+// hold types
+
+SWEP.HoldType = "revolver"
+SWEP.HoldTypeSprint = "normal"
+SWEP.HoldTypeBlindFire = "pistol"
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_PISTOL
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_PISTOL
+
+SWEP.PassiveAng = Angle(0, 0, 0)
+SWEP.PassivePos = Vector(0, -2, -5)
+
+SWEP.BlindFireAng = Angle(0, 5, 0)
+SWEP.BlindFirePos = Vector(0, -2, -5)
+
+SWEP.SprintAng = Angle(0, 30, 0)
+SWEP.SprintPos = Vector(2, 0, -12)
+
+SWEP.SightAng = Angle(0, -0.1, 0)
+SWEP.SightPos = Vector(-3.25, 0, -3.77)
+
+SWEP.Sway = 2
+SWEP.ScopedSway = 0
+
+// reload
+
+SWEP.ClipSize = 7
+SWEP.Ammo = "pistol"
+
+SWEP.ReloadTimeMult = 1
+
+SWEP.DropMagazineModel = "models/weapons/tacint/magazines/gsr1911.mdl"
+SWEP.DropMagazineImpact = "pistol"
+
+// sounds
+
+local path = "tacint_extras/m1911/"
+local path2 = "tacint/weapons/gsr1911/gsr1911_"
+
+SWEP.Sound_Shoot = "^" .. path .. "fire-1.wav"
+SWEP.Sound_Shoot_Silenced = path2 .. "fire_silenced-1.wav"
+
+SWEP.Vol_Shoot = 130
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 4
+
+SWEP.MuzzleEffect = "muzzleflash_pistol"
+
+// anims
+
+// VM:
+// idle
+// fire
+// fire1, fire2...
+// dryfire
+// melee
+// reload
+// midreload
+// prime_grenade
+// throw_grenade
+// throw_grenade_underhand
+// deploy
+// blind_idle
+// blind_fire
+// blind_fire1, blind_fire2...
+// blind_dryfire
+
+// WM:
+// attack1
+SWEP.AnimationTranslationTable = {
+    ["deploy"] = "draw",
+    ["fire"] = {"shoot1", "shoot2", "shoot3"},
+    ["blind_fire"] = {"blind_shoot1", "blind_shoot2", "blind_shoot3"},
+    ["melee"] = {"melee1", "melee2"}
+}
+
+SWEP.LastShot = true
+
+// attachments
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Muzzle",
+        Category = {"silencer"},
+        Bone = "ValveBiped.barrel",
+        WMBone = "Box01",
+        AttachSound = "tacint/weapons/silencer_on.wav",
+        DetachSound = "tacint/weapons/silencer_off.wav",
+        VMScale = 0.5,
+        WMScale = 0.5,
+        Pos_VM = Vector(-0.76, 0.7, 7.5),
+        Ang_VM = Angle(90, 0, 0),
+        Pos_WM = Vector(0, 9.25, -1.5),
+        Ang_WM = Angle(0, -90, 0),
+    },
+    [2] = {
+        PrintName = "Perk",
+        Category = {"perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacint/weapons/flashlight_on.wav",
+        DetachSound = "tacint/weapons/flashlight_off.wav",
+    },
+}
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_extras_m1911.clip_in", path .. "clipin.mp3")
+addsound("tacint_extras_m1911.clip_in-mid", path2 .. "clip_in-mid.wav")
+addsound("tacint_extras_m1911.clip_out", path .. "clipout.mp3")
+addsound("tacint_extras_m1911.slide_action", path2 .. "slide_action.wav")
+addsound("tacint_extras_m1911.slide_shut", path2 .. "slide_shut.wav")
+addsound("tacint_extras_m1911.cock_hammer", path2 .. "cockhammer.wav")
