bf582a86a8959383a01ed3d408ff3aa2c14baf2a
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-02-24 13:20:43 -0500

    oospa comp

---
+++ lua/tacint/shared/atts/muzz_comp_usp.lua
@@ -0,0 +1,14 @@
+ATT.PrintName = "Compensator"
+ATT.Icon = Material("entities/tacrp_att_muzz_pistol_comp.png", "mips smooth")
+ATT.Description = "Muzzle device which reduces recoil impact."
+ATT.Pros = {"Recoil"}
+ATT.Cons = {"Fire rate"}
+
+ATT.Category = "comp_usp" // can be "string" or {"list", "of", "strings"}
+ATT.InvAtt = "muzz_pistol_comp"
+ATT.SortOrder = 1
+
+ATT.Mult_RecoilKick = 0.5
+ATT.Mult_RPM = 0.9
+
+ATT.InstalledElements = {"pistol_comp"}
