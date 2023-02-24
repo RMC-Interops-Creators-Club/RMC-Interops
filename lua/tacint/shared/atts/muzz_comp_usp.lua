9b3b4f17f7497cfa4fce8fc60509e98adf044fbf
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-02-24 13:22:32 -0500

    yeah i'm retarded

--- lua/tacint/shared/atts/muzz_comp_usp.lua
+++ lua/tacint/shared/atts/muzz_comp_usp.lua
@@ -1,5 +1,5 @@
 ATT.PrintName = "Compensator"
-ATT.Icon = Material("entities/tacrp_att_muzz_pistol_comp.png", "mips smooth")
+ATT.Icon = Material("entities/tacint_att_muzz_pistol_comp.png", "mips smooth")
 ATT.Description = "Muzzle device which reduces recoil impact."
 ATT.Pros = {"Recoil"}
 ATT.Cons = {"Fire rate"}
@@ -9,6 +9,6 @@ ATT.InvAtt = "muzz_pistol_comp"
 ATT.SortOrder = 1
 
 ATT.Mult_RecoilKick = 0.5
-ATT.Mult_RPM = 0.9
+ATT.Mult_RPM = 0.95
 
 ATT.InstalledElements = {"pistol_comp"}
