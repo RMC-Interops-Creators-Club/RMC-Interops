8032c0787e28f69056093bc0bdb92390b6e0382c
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-02-25 09:28:44 -0500

    Update cl_worldmodel.lua
    prevent the console from spamming errors if you drop a weapon that has a laser equipped

--- lua/weapons/tacint_base/cl_worldmodel.lua
+++ lua/weapons/tacint_base/cl_worldmodel.lua
@@ -1,6 +1,6 @@
 function SWEP:DrawWorldModel()
     self:DrawCustomModel(true)
-    self:DrawLasers(true)
+    self:DrawLasers(false)
 
     if self:GetValue("Laser") then
         self:SetRenderBounds(Vector(-16, -16, -16), Vector(16, 16, 15000))
