b146e3511d21c547f39fbeaa65c4076af37605bb
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-02-25 18:28:43 -0500

    ak47 sniper wip

---
+++ lua/weapons/tacint_extras_vss.lua
@@ -0,0 +1,211 @@
+SWEP.Base = "tacint_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "VSS Vintorez"
+SWEP.Category = "Tactical Intervention Extras (RMC)"
+
+SWEP.Description = "Integrally-suppressed sniper rifle with good accuracy."
+
+SWEP.ViewModel = "models/weapons/tacint_extras/v_vss.mdl"
+SWEP.WorldModel = "models/weapons/tacint/w_ak47.mdl"
+
+SWEP.Slot = 3
+
+// "ballistics"
+
+SWEP.Damage_Max = 100 / 3 // damage at minimum range
+SWEP.Damage_Min = 100 / 5 // damage at maximum range
+SWEP.Range_Min = 2500 // distance for which to maintain maximum damage
+SWEP.Range_Max = 6000 // distance at which we drop to minimum damage
+SWEP.Penetration = 3 // units of metal this weapon can penetrate
+
+// misc. shooting
+
+SWEP.Firemode = 1
+
+SWEP.RPM = 600
+
+SWEP.Spread = 0.007
+
+// Spread penalties are in spread units and are additive
+SWEP.MoveSpreadPenalty = 0.055 // spread penalty while travelling at max. 250 u/s
+SWEP.MidAirSpreadPenalty = 0.12 // spread penalty for being in the air
+SWEP.HipFireSpreadPenalty = 0.003 // spread penalty for not being scoped in
+SWEP.ScopedSpreadPenalty = 0
+SWEP.BlindFireSpreadPenalty = 0.035 // spread penalty for blind firing
+SWEP.CrouchSpreadPenalty = -0.003
+
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 10
+SWEP.RecoilResetTime = 0.2 // time after you stop shooting for recoil to start dissipating
+SWEP.RecoilDissipationRate = 100
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 0.15
+SWEP.RecoilVisualShake = 0.35
+
+SWEP.RecoilKick = 1
+
+SWEP.RecoilSpreadPenalty = 0.0035 // extra spread per one unit of recoil
+
+SWEP.CanBlindFire = true
+
+// handling
+
+SWEP.MoveSpeedMult = 0.94
+SWEP.ShootingSpeedMult = 1 // slow down applied while shooting
+SWEP.SightedSpeedMult = 0.5
+SWEP.MeleeSpeedMult = 1
+SWEP.MeleeSpeedMultTime = 2 // seconds to apply slow down for
+
+SWEP.SprintToFireTime = 0.33 // multiplies how long it takes to recover from sprinting
+
+// hold types
+
+SWEP.HoldType = "ar2"
+SWEP.HoldTypeSprint = "passive"
+SWEP.HoldTypeBlindFire = false
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_SMG1
+
+SWEP.PassiveAng = Angle(0, 0, 0)
+SWEP.PassivePos = Vector(0, -2, -4)
+
+SWEP.BlindFireAng = Angle(0, 5, 0)
+SWEP.BlindFirePos = Vector(0, -2, -4)
+
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, 0, -2)
+
+// scope
+
+SWEP.Scope = true
+SWEP.ScopeOverlay = Material("tacint/scopes/sniper.png", "mips smooth") // Material("path/to/overlay")
+SWEP.ScopeFOV = 90 / 5
+SWEP.ScopeLevels = 1
+
+SWEP.QuickScopeSpreadPenalty = 0.5
+SWEP.QuickScopeTime = 0.3 // amount of time over which to fade out the quickscope spread penalty
+
+
+SWEP.ClipSize = 10
+SWEP.Ammo = "ti_762wp"
+
+SWEP.ReloadTimeMult = 1
+SWEP.DropMagazineModel = "models/weapons/tacint/magazines/ak47.mdl"
+SWEP.DropMagazineImpact = "metal"
+
+// sounds
+
+local path = "tacint_extras/vss/"
+local path1 = "tacint_extras/ak47/"
+
+SWEP.Sound_Shoot = "^" .. path .. "fire.wav"
+
+SWEP.Vol_Shoot = 70
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 1
+// ditto for shell
+SWEP.QCA_Eject = 2
+
+SWEP.MuzzleEffect = "muzzleflash_ak47"
+SWEP.EjectEffect = 2
+
+// anims
+
+// VM:
+// idle
+// fire
+// fire1, fire2...
+// dryfire
+// melee
+// reload
+// midreload
+// prime_grenade
+// throw_grenade
+// throw_grenade_underhand
+// deploy
+// blind_idle
+// blind_fire
+// blind_fire1, blind_fire2...
+// blind_dryfire
+
+// WM:
+// attack1
+SWEP.AnimationTranslationTable = {
+    ["deploy"] = "deploy",
+    ["fire1"] = "fire1_M",
+    ["fire2"] = "fire2_M",
+    ["fire3"] = "fire3_M",
+    ["fire4"] = "fire4_M",
+    ["fire5"] = "fire5_M",
+    ["melee"] = {"melee1", "melee2"}
+}
+
+// attachments
+
+SWEP.AttachmentElements = {
+    ["tactical"] = {
+        BGs_VM = {
+            {2, 1}
+        },
+    },
+}
+
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Optic",
+        Category = {"optic_cqb", "optic_medium", "optic_sniper"},
+        Bone = "ValveBiped.AK47_rootbone",
+        WMBone = "Box01",
+        InstalledElements = {"tactical"},
+        AttachSound = "tacint/weapons/optic_on.wav",
+        DetachSound = "tacint/weapons/optic_off.wav",
+        VMScale = 0.75,
+        Pos_VM = Vector(-5.15, 0.15, 4),
+        Ang_VM = Angle(90, 0, 0),
+        Pos_WM = Vector(0, 3, 0.5),
+        Ang_WM = Angle(0, -90, 0),
+    },
+    [2] = {
+        PrintName = "Tactical",
+        Category = "tactical",
+        Bone = "ValveBiped.AK47_rootbone",
+        WMBone = "Box01",
+        AttachSound = "tacint/weapons/flashlight_on.wav",
+        DetachSound = "tacint/weapons/flashlight_off.wav",
+        Pos_VM = Vector(-3.25, -0.1, 19),
+        Pos_WM = Vector(0, 19, -2),
+        Ang_VM = Angle(90, 0, -90),
+        Ang_WM = Angle(0, -90, 180),
+    },
+    [3] = {
+        PrintName = "Perk",
+        Category = {"perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacint/weapons/flashlight_on.wav",
+        DetachSound = "tacint/weapons/flashlight_off.wav",
+    },
+}
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_extras_vss.remove_clip", path .. "clipin1.wav")
+addsound("tacint_extras_vss.insert_clip", path .. "clipin2.wav")
+addsound("tacint_extras_vss.boltaction", path1 .. "bolt.mp3")
+addsound("tacint_extras_vss.Buttstock_Back", path .. "buttstock_back.wav")
