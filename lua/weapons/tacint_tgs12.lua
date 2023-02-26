e085cb978010ba4a8b546cd8c35aef4dd81992d9
Fesiug <publicfesiug@outlook.com>
2023-02-26 14:19:24 -0500

    Update tacint_tgs12.lua

--- lua/weapons/tacint_tgs12.lua
+++ lua/weapons/tacint_tgs12.lua
@@ -16,8 +16,8 @@ SWEP.Slot = 3
 
 // "ballistics"
 
-SWEP.Damage_Max = 100 / 4 // damage at minimum range
-SWEP.Damage_Min = 100 / 8 // damage at maximum range
+SWEP.Damage_Max = 100 / 8 // damage at minimum range
+SWEP.Damage_Min = 100 / 10 // damage at maximum range
 SWEP.Range_Min = 1000 // distance for which to maintain maximum damage
 SWEP.Range_Max = 3000 // distance at which we drop to minimum damage
 SWEP.Penetration = 0.25 // units of metal this weapon can penetrate
@@ -40,13 +40,13 @@ SWEP.Firemode = 1
 
 SWEP.RPM = 75
 
-SWEP.Spread = 0.075
+SWEP.Spread = 0.2
 
 SWEP.ShootSpeedMult = 1
 
 // Spread penalties are in spread units and are additive
-SWEP.MoveSpreadPenalty = 0.08 // spread penalty while travelling at max. 250 u/s
-SWEP.MidAirSpreadPenalty = 0.12 // spread penalty for being in the air
+SWEP.MoveSpreadPenalty = 0.16 // spread penalty while travelling at max. 250 u/s
+SWEP.MidAirSpreadPenalty = 0.16 // spread penalty for being in the air
 SWEP.HipFireSpreadPenalty = 0 // spread penalty for not being scoped in
 SWEP.BlindFireSpreadPenalty = 0.05 // spread penalty for blind firing
 SWEP.CrouchSpreadPenalty = 0
@@ -98,7 +98,7 @@ SWEP.SprintPos = Vector(5, 0, -2)
 
 // reload
 
-SWEP.ClipSize = 7
+SWEP.ClipSize = 4
 SWEP.Ammo = "ti_12g"
 SWEP.ShotgunReload = true
 
