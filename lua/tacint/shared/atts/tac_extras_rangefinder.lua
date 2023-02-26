74994ec13aa9c9287954e1f7a08750c20520d06e
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-02-26 16:56:12 -0500

    Update tac_extras_rangefinder.lua

--- lua/tacint/shared/atts/tac_extras_rangefinder.lua
+++ lua/tacint/shared/atts/tac_extras_rangefinder.lua
@@ -1,8 +1,12 @@
-ATT.PrintName = "Ballistics CPU"
-ATT.FullName = "Tactical Ballistics CPU"
+ATT.PrintName = "Rangefinder"
+ATT.FullName = "Tactical Ballistics Trajectory Computer"
 ATT.Icon = Material("entities/tacrp_att_tac_extras_rangefinder.png", "mips smooth")
 ATT.Description = "High-tech tactical CPU that calculates ballistics and battle information."
+<<<<<<< Updated upstream
 ATT.Pros = {"Display damage at range", "Rangefinder"}
+=======
+ATT.Pros = {"Display range and shots-to-kill"}
+>>>>>>> Stashed changes
 
 ATT.Model = "models/weapons/tacint/addons/rangefinder_mounted.mdl"
 ATT.Scale = 1
