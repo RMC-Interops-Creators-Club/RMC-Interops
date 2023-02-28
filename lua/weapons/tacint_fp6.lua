fd03220ffab6f9435406db2a4366549332b2d6eb
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-02-27 21:48:44 -0500

    shotta guna

--- lua/weapons/tacint_fp6.lua
+++ lua/weapons/tacint_fp6.lua
@@ -40,14 +40,14 @@ SWEP.Firemode = 1
 
 SWEP.RPM = 80
 
-SWEP.Spread = 0.04
+SWEP.Spread = 0
 
 SWEP.ShootSpeedMult = 1
 
 // Spread penalties are in spread units and are additive
 SWEP.MoveSpreadPenalty = 0.08 // spread penalty while travelling at max. 250 u/s
 SWEP.MidAirSpreadPenalty = 0.12 // spread penalty for being in the air
-SWEP.HipFireSpreadPenalty = 0.05 // spread penalty for not being scoped in
+SWEP.HipFireSpreadPenalty = 0.085 // spread penalty for not being scoped in
 SWEP.BlindFireSpreadPenalty = 0.05 // spread penalty for blind firing
 SWEP.CrouchSpreadPenalty = 0
 SWEP.ScopedSpreadPenalty = 0
