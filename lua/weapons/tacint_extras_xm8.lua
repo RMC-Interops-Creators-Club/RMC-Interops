79c08d52028babd31db9c2892e8dadcef5849dcf
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-02-28 16:20:29 -0500

    xm8 lmg
    needs anim edits and all the other stuff still

---
+++ lua/weapons/tacint_extras_xm8.lua
@@ -0,0 +1,233 @@
+SWEP.Base = "tacint_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "XM8 LMG"
+SWEP.Category = "RMC Interops (Heavy Weapons)"
+
+SWEP.Description = "Experimental light machine gun with an inegral scope and carbine-length profile."
+
+SWEP.ViewModel = "models/weapons/tacint_extras/v_xm8lmg.mdl"
+SWEP.WorldModel = "models/weapons/tacint/w_g36k.mdl"
+
+SWEP.Slot = 2
+
+// "ballistics"
+
+SWEP.Damage_Max = 100 / 4 // damage at minimum range
+SWEP.Damage_Min = 100 / 8 // damage at maximum range
+SWEP.Range_Min = 2000 // distance for which to maintain maximum damage
+SWEP.Range_Max = 5000 // distance at which we drop to minimum damage
+SWEP.Penetration = 2 // units of metal this weapon can penetrate
+
+// misc. shooting
+
+SWEP.Firemode = 2
+
+SWEP.RPM = 750
+
+SWEP.Spread = 0
+
+// Spread penalties are in spread units and are additive
+SWEP.MoveSpreadPenalty = 0.045 // spread penalty while travelling at max. 250 u/s
+SWEP.MidAirSpreadPenalty = 0.1 // spread penalty for being in the air
+SWEP.HipFireSpreadPenalty = 0.04 // spread penalty for not being scoped in
+SWEP.ScopedSpreadPenalty = 0.01
+SWEP.BlindFireSpreadPenalty = 0.05 // spread penalty for blind firing
+SWEP.CrouchSpreadPenalty = -0.002
+
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 20
+SWEP.RecoilResetTime = 0.25 // time after you stop shooting for recoil to start dissipating
+SWEP.RecoilDissipationRate = 100
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 0.15
+SWEP.RecoilVisualShake = 0.25
+
+SWEP.RecoilKick = 0.35
+
+SWEP.RecoilSpreadPenalty = 0.006 // extra spread per one unit of recoil
+
+SWEP.CanBlindFire = true
+
+// handling
+
+SWEP.MoveSpeedMult = 0.92
+SWEP.ShootingSpeedMult = 1 // slow down applied while shooting
+SWEP.SightedSpeedMult = 0.5
+SWEP.MeleeSpeedMult = 1
+SWEP.MeleeSpeedMultTime = 2 // seconds to apply slow down for
+
+SWEP.SprintToFireTime = 0.45 // multiplies how long it takes to recover from sprinting
+
+// hold types
+
+SWEP.HoldType = "ar2"
+SWEP.HoldTypeSprint = "passive"
+SWEP.HoldTypeBlindFire = false
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_SMG1
+
+SWEP.PassiveAng = Angle(0, 0, 0)
+SWEP.PassivePos = Vector(0, -2, -5)
+
+SWEP.BlindFireAng = Angle(0, 5, 0)
+SWEP.BlindFirePos = Vector(0, -2, -4)
+
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, 0, -2)
+
+// scope
+
+SWEP.Scope = true
+SWEP.ScopeOverlay = Material("tacint/scopes/g36.png", "mips smooth") // Material("path/to/overlay")
+SWEP.ScopeFOV = 90 / 2
+SWEP.ScopeLevels = 1 // 2 = like CS:S
+
+SWEP.QuickScopeSpreadPenalty = 0.01
+SWEP.QuickScopeTime = 0.1 // amount of time over which to fade out the quickscope spread penalty
+
+// reload
+
+SWEP.ClipSize = 75
+SWEP.Ammo = "ar2"
+
+SWEP.ReloadTimeMult = 1.32
+SWEP.DropMagazineModel = "models/weapons/tacint/magazines/g36k.mdl"
+SWEP.DropMagazineImpact = "plastic"
+
+// sounds
+
+local path = "tacint/weapons/g36k/g36k_"
+local path1 = "tacint_extras/xm8/"
+
+SWEP.Sound_Shoot = "^" .. path1 .. "m249-1.wav"
+SWEP.Sound_Shoot_Silenced = path .. "fire_silenced-1.wav"
+
+SWEP.Vol_Shoot = 110
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 1
+// ditto for shell
+SWEP.QCA_Eject = 2
+
+SWEP.MuzzleEffect = "muzzleflash_ak47"
+SWEP.EjectEffect = 2
+
+// anims
+
+// VM:
+// idle
+// fire
+// fire1, fire2...
+// dryfire
+// melee
+// reload
+// midreload
+// prime_grenade
+// throw_grenade
+// throw_grenade_underhand
+// deploy
+// blind_idle
+// blind_fire
+// blind_fire1, blind_fire2...
+// blind_dryfire
+
+// WM:
+// attack1
+SWEP.AnimationTranslationTable = {
+    ["fire1"] = "fire1_M",
+    ["fire2"] = "fire2_M",
+    ["fire3"] = "fire3_M",
+    ["fire4"] = "fire4_M",
+    ["fire5"] = "fire5_M",
+    ["melee"] = {"melee1", "melee2"}
+}
+
+// attachments
+
+SWEP.AttachmentElements = {
+    ["foldstock"] = {
+        BGs_VM = {
+            {1, 1}
+        },
+        BGs_WM = {
+            {1, 1}
+        }
+    },
+    ["tactical"] = {
+        BGs_VM = {
+            {2, 1}
+        },
+    },
+}
+
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Optic",
+        Category = "ironsights",
+        Bone = "ValveBiped.g36k_rootbone",
+        WMBone = "Box01",
+        AttachSound = "tacint/weapons/optic_on.wav",
+        DetachSound = "tacint/weapons/optic_off.wav",
+        VMScale = 0.75,
+        Pos_VM = Vector(-7.3, 0.25, 2),
+        Pos_WM = Vector(0, 0, 2.75),
+        Ang_VM = Angle(90, 0, 0),
+        Ang_WM = Angle(0, -90, 0),
+    },
+    [2] = {
+        PrintName = "Muzzle",
+        Category = "silencer",
+        Bone = "ValveBiped.g36k_rootbone",
+        WMBone = "Box01",
+        AttachSound = "tacint/weapons/silencer_on.wav",
+        DetachSound = "tacint/weapons/silencer_off.wav",
+        Pos_VM = Vector(-3.6, 0.5, 26),
+        Pos_WM = Vector(-0.25, 24, -1),
+        Ang_VM = Angle(90, 0, 0),
+        Ang_WM = Angle(0, -90, 0),
+    },
+    [3] = {
+        PrintName = "Tactical",
+        Category = "tactical",
+        Bone = "ValveBiped.g36k_rootbone",
+        WMBone = "Box01",
+        AttachSound = "tacint/weapons/flashlight_on.wav",
+        DetachSound = "tacint/weapons/flashlight_off.wav",
+        InstalledElements = {"tactical"},
+        Pos_VM = Vector(-3.75, -0.8, 17),
+        Pos_WM = Vector(0.9, 15, -1),
+        Ang_VM = Angle(90, 0, -80),
+        Ang_WM = Angle(-70, -90, 0),
+    },
+    [4] = {
+        PrintName = "Perk",
+        Category = {"foldstock", "perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacint/weapons/flashlight_on.wav",
+        DetachSound = "tacint/weapons/flashlight_off.wav",
+    },
+}
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_xm8.remove_clip", path1 .. "magout.wav")
+addsound("tacint_xm8.insert_clip", path1 .. "magin.wav")
+addsound("tacint_xm8.bolt_action", path .. "bolt_action.wav")
+addsound("tacint_xm8.fire_select", path .. "fire_selector.wav")
+addsound("tacint_xm8.Buttstock_Back", path .. "buttstock_back.wav")
