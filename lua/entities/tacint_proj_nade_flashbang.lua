9db9c09a893507b8a41737bfcf94c21f5d74985c
Fesiug <publicfesiug@outlook.com>
2023-02-28 21:41:10 -0500

    get stunned nerd

--- lua/entities/tacint_proj_nade_flashbang.lua
+++ lua/entities/tacint_proj_nade_flashbang.lua
@@ -27,6 +27,27 @@ ENT.ExplodeSounds = {
     "tacint/weapons/grenade/flashbang_explode-1.wav",
 }
 
+if SERVER then
+	util.AddNetworkString("tacintrmc_getflashed")
+	util.AddNetworkString("tacintrmc_getstunned")
+else
+	local fade = (1/8)
+	local fcolor = Color( 230, 230, 230 )
+	net.Receive("tacintrmc_getflashed", function()
+		local ply = LocalPlayer()
+		local time = net.ReadFloat()
+		assert( time, "Amount of time to be flashed NOT defined" )
+		ply:ScreenFade( SCREENFADE.OUT, fcolor, fade, 1 )
+		timer.Simple( fade, function()
+			ply:ScreenFade( SCREENFADE.IN, fcolor, 2, time )
+		end)
+	end)
+	net.Receive("tacintrmc_getstunned", function()
+		local ply = LocalPlayer()
+		ply:SetDSP( 37, false )
+	end)
+end
+
 function ENT:Detonate()
     util.BlastDamage(self, self:GetOwner(), self:GetPos(), 150, 25)
 
@@ -56,10 +77,12 @@ function ENT:Detonate()
             time = Lerp( dist / flashpower, time, 0 )
 
             if k:VisibleVec( flashorigin ) then
-                k:ScreenFade( SCREENFADE.IN, Color( 255, 255, 255, 255 ), 2.5, time )
+				net.Start("tacintrmc_getflashed")
+					net.WriteFloat(time)
+				net.Send(k)
             end
-
-            k:SetDSP( 37, false )
+			net.Start("tacintrmc_getstunned")
+			net.Send(k)
         end
     end
 
