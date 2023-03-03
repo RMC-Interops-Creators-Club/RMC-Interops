597ec63f379a27eaff3734c6e179511744350431
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-03-03 10:53:10 -0500

    RIP rounds experiment

---
+++ lua/tacint/shared/atts/acc_rip.lua
@@ -0,0 +1,18 @@
+ATT.PrintName = "RIP"
+ATT.FullName = "Radically Invasive Projectile"
+ATT.Icon = Material("entities/tacint_att_acc_hollowpoints.png", "mips smooth")
+ATT.Description = "Special pistol rounds that improve damage but worsen damage falloff."
+ATT.Pros = {"Increased damage"}
+ATT.Cons = {"Faster damage dropoff", "80% clip capacity"}
+
+ATT.InstalledElements = {"smallmag"}
+
+ATT.Category = "perk_rip" // can be "string" or {"list", "of", "strings"}
+
+ATT.SortOrder = 1
+
+ATT.Mult_ClipSize = (4/5)
+ATT.Mult_Range_Max = 0.8
+ATT.Mult_Range_Min = 0.8
+ATT.Mult_Damage_Min = 0.7
+ATT.Mult_Damage_Max = 1.5
