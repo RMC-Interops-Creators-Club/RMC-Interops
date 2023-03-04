89093524beb54e6014da2edb1ced08791f8a4fe8
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-03-04 16:35:12 -0500

    .

--- lua/tacint/shared/atts/acc_blindfire.lua
+++ lua/tacint/shared/atts/acc_blindfire.lua
@@ -1,7 +1,7 @@
 ATT.PrintName = "Blindfire+"
 ATT.FullName = "Blindfire Training"
 ATT.Icon = Material("entities/tacint_att_acc_blindfire.png", "mips smooth")
-ATT.Description = "Handling techniques that improve weapon control in blindfire."
+ATT.Description = "Handling techniques that improve weapon control in blindfire"
 ATT.Pros = {"No blindfire spread penalty."}
 
 ATT.Category = {"perk_shooting", "perk_blindfire"} // can be "string" or {"list", "of", "strings"}
