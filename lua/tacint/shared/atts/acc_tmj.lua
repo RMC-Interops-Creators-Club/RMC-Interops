80c0047f5eb6b66ce48be07796e363b13419c385
Jayrazer <79886161+Jayrazer@users.noreply.github.com>
2023-03-04 13:47:38 -0500

    Update acc_tmj.lua

--- lua/tacint/shared/atts/acc_tmj.lua
+++ lua/tacint/shared/atts/acc_tmj.lua
@@ -1,13 +1,11 @@
 ATT.PrintName = "TMJ"
 ATT.FullName = "Total Metal Jacket Rounds"
 ATT.Icon = Material("entities/tacint_att_acc_tmj.png", "mips smooth")
-ATT.Description = "Copper rounds that increase penetration but reduce stopping power."
+ATT.Description = "Copper-jacketed rounds that increase penetration."
 ATT.Pros = {"Increased penetration"}
-ATT.Cons = {"Reduced max damage"}
 
 ATT.Category = "perk_tmj" // can be "string" or {"list", "of", "strings"}
 
 ATT.SortOrder = 1
 
 ATT.Mult_Penetration = 4
-ATT.Mult_Damage_Max = 0.9
