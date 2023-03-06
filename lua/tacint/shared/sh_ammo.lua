b164af363c4fc1aa28b973aed0256f161a18756a
Fesiug <publicfesiug@outlook.com>
2023-03-05 21:51:12 -0500

    remove unused ammo

--- lua/tacint/shared/sh_ammo.lua
+++ lua/tacint/shared/sh_ammo.lua
@@ -4,21 +4,7 @@ local ammotypes = {
     ["ti_smoke"] = "Smoke Grenades",
     ["ti_c4"] = "C4 Charges",
     ["ti_gas"] = "Gas Grenades",
-    ["ti_9mm"] = "9mm Parabellum",
-    ["ti_45acp"] = ".45 ACP",
-    ["ti_12g"] = "12 Gauge",
-    ["ti_556"] = "5.56mm NATO",
-    ["ti_357"] = ".357 Magnum",
-    ["ti_rpg7"] = "RPG-7 Rocket",
-    ["ti_40mm"] = "40mm Grenade",
-    ["ti_32acp"] = ".32 Auto",
-    ["ti_50bmg"] = ".50 BMG",
-    ["ti_308"] = ".308 Winchester",
     ["ti_nuke"] = "Nuclear Device",
-    ["ti_762nato"] = "7.62mm NATO",
-    ["ti_762wp"] = "7.62x39mm WP",
-    ["ti_47mm"] = "4.7mm HK",
-    ["ti_57mm"] = "5.7mm FN"
 }
 
 for i, k in pairs(ammotypes) do
