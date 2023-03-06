f17573257b4bb1b3ef989f801aa07492a12a0266
Fesiug <publicfesiug@outlook.com>
2023-03-05 21:53:44 -0500

    new stats code or whatever

--- lua/weapons/tacint_base/sh_stats.lua
+++ lua/weapons/tacint_base/sh_stats.lua
@@ -6,6 +6,11 @@ SWEP.ExcludeFromRawStats = {
     ["Description"] = true
 }
 
+SWEP.IntegerStats = {
+    ["ClipSize"] = true,
+    ["Num"] = true,
+}
+
 function SWEP:RunHook(val, data)
     if self.HookCache[val] then
         for _, chook in pairs(self.HookCache[val]) do
@@ -42,74 +47,82 @@ function SWEP:RunHook(val, data)
     return data
 end
 
-function SWEP:GetValue(val)
-    local tbl = self:GetTable()
+local emptt = {}
 
-    local stat = tbl[val]
+function SWEP:GetValue(val, invert)
+	local tbl = self:GetTable()
 
-    if self.StatCache[val] then
-        return self.StatCache[val]
-    end
+	local stat = tbl[val]
 
-    local priority = 0
+	if !invert and self.StatCache[val] then
+		return self.StatCache[val]
+	end
 
-    if !self.ExcludeFromRawStats[val] then
-        for slot, slottbl in pairs(self.Attachments) do
-            if !slottbl.Installed then continue end
+	local priority = 0
 
-            local atttbl = TacInt.GetAttTable(slottbl.Installed)
+	local totalstatbanks = {}
+	for slot, slottbl in pairs(self.Attachments) do
+		if !slottbl.Installed then continue end
+		table.insert(totalstatbanks, TacInt.GetAttTable(slottbl.Installed))
+	end
 
-            local att_priority = atttbl["Priority_" .. val] or 1
+	for slot, slottbl in pairs(self.AttachmentElements) do
+		local eles = {}
+		for i, k in pairs(self.Attachments) do
+			if k.Installed then
+				table.Add(eles, k.InstalledElements or emptt)
+				local atttbl = TacInt.GetAttTable(k.Installed)
+				table.Add(eles, atttbl.InstalledElements or emptt)
+			else
+				table.Add(eles, k.UnInstalledElements or emptt)
+			end
+		end
+		PrintTable(eles)
+		for index, fucker in ipairs(eles) do
+			if slot != fucker then continue end
+			table.insert(totalstatbanks, slottbl)
+		end
+	end
 
-            if atttbl[val] != nil and att_priority > priority then
-                stat = atttbl[val]
-                priority = att_priority
-            end
-        end
-    end
+	if !self.ExcludeFromRawStats[val] then
+		for slot, slottbl in pairs(totalstatbanks) do
+			local att_priority = slottbl["Priority_" .. val] or 1
 
-    for slot, slottbl in pairs(self.Attachments) do
-        if !slottbl.Installed then continue end
+			if slottbl[val] != nil and att_priority > priority then
+				stat = slottbl[val]
+				priority = att_priority
+			end
+		end
+	end
 
-        local atttbl = TacInt.GetAttTable(slottbl.Installed)
+	for slot, slottbl in pairs(totalstatbanks) do
+		local att_priority = slottbl["Override_Priority_" .. val] or 1
 
-        local att_priority = atttbl["Override_Priority_" .. val] or 1
+		if slottbl["Override_" .. val] != nil and att_priority > priority then
+			stat = slottbl["Override_" .. val]
+			priority = att_priority
+		end
 
-        if atttbl["Override_" .. val] != nil and att_priority > priority then
-            stat = atttbl["Override_" .. val]
-            priority = att_priority
-        end
-    end
+		if isnumber(stat) and slottbl["Add_" .. val] then
+			stat = stat + slottbl["Add_" .. val] * (invert and -1 or 1)
+		end
 
-    for slot, slottbl in pairs(self.Attachments) do
-        if !slottbl.Installed then continue end
+		if isnumber(stat) and slottbl["Mult_" .. val] then
+			if invert then
+				stat = stat / slottbl["Mult_" .. val]
+			else
+				stat = stat * slottbl["Mult_" .. val]
+			end
+		end
+	end
 
-        local atttbl = TacInt.GetAttTable(slottbl.Installed)
+	if self.IntegerStats[val] then
+		stat = math.ceil(stat)
+	end
 
-        if stat then
-            if atttbl["Add_" .. val] then
-                stat = stat + atttbl["Add_" .. val]
-            end
-        end
-    end
+	if !invert then
+		self.StatCache[val] = stat
+	end
 
-    for slot, slottbl in pairs(self.Attachments) do
-        if !slottbl.Installed then continue end
-
-        local atttbl = TacInt.GetAttTable(slottbl.Installed)
-
-        if stat then
-            if atttbl["Mult_" .. val] then
-                stat = stat * atttbl["Mult_" .. val]
-            end
-        end
-    end
-
-    if GetConVar("tacint_hl2ammo"):GetBool() and val == "Ammo" then
-        stat = self.AmmoToHL2Translate[stat] or stat
-    end
-
-    self.StatCache[val] = stat
-
-    return stat
+	return stat
 end
